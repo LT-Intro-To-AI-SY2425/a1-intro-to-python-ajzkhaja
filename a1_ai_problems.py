1d580727a7de557bc1164c0d53a98f893416c11c
Aijaz Khaja <Akhaja@cps.edu>
2024-09-20 11:23:33 -0500

    I did 5 problems

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -17,14 +17,44 @@ assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
 assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
 
-print('hello world')
+print('hello world')    
 
+#Assigning variables then calling them  
 name = 'roger'
 name2 = 'Sam'
 name3 = 'Alex'
 print(name)
 print(name2)
-print(name3)      
+print(name3)  
+
+
+# Count from 1 to 10
+for number in range(1, 11):
+    print(number)
+
+    #Word Count 
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+word_count = len(words)
+
+print(f"The number of words in the sentence is: {word_count}")
+
+
+#Odd or Even 
+number = int(input("Enter an integer: "))
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
+ #Muliplication calculator
+num1, num2 = map(float, input("Enter two numbers: ").split())
+print(f"Product: {num1 * num2}")
+
+
+
+
+
 
 
 
